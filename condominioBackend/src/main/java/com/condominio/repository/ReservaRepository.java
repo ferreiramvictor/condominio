6210a4d6a11d95ed867c782398c7ae80d0872cec
Victor Marques <naotemerro@hotmail.com>
2025-05-25 18:40:48 -0300

    subindo

--- condominioBackend/src/main/java/com/condominio/repository/ReservaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.condominio.repository;
-
-import com.condominio.model.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservaRepository extends JpaRepository<Reserva, Long> {
-    // Métodos customizados (se necessário)
-}
